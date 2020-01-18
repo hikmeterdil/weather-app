0d067db3011eede1fdea38b0d83f69e2fa86fc36
hikmeterdil <hikmetrdl@gmail.com>
2020-01-18 13:50:15 +0100

    finished week1 project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import {Weather} from './components/Weather'
+
+function App() {
+  return (
+    <div className="App">
+    <h2 className="title">Weather</h2>
+    <Weather/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Weather.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import data from './city-weather.json';
+export function Weather() {
+
+return(
+    <div className="weather-cont">
+{data.map(item =>
+<div className='weather'>
+<h2 className="name" key={item.sys.id}>{item.name}, {item.sys.country}</h2>
+<h3 className="condition" key={item.sys.id}>{item.weather[0].main}</h3>
+<p className="condition" key={item.sys.id}>{item.weather[0].description}</p>
+<p className="para" key={item.sys.id}>{`min-temp: ${item.main.temp_min}`}</p>
+<p className="para" key={item.sys.id}>{`max-temp: ${item.main.temp_max}`}</p>
+<p className="para" key={item.sys.id}>{`location: ${item.coord.lon}, ${item.coord.lat}`}</p>
+</div>
+)}
+</div>
+)};
