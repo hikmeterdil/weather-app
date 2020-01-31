bf47bf0da3902da922177ca32d3321b9211e920c
hikmeterdil <hikmetrdl@gmail.com>
2020-01-31 15:52:34 +0100

    minor fixes

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,36 +2,31 @@ import React from "react";
 import { useState } from "react";
 import { Search } from "./Search";
 import { City } from "./City";
-
 export function Weather() {
   const [inputValue, setInputValue] = useState("");
   const [cityData, setCityData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [hasError, setHasError] = useState(false);
-
   const fetchData = async inputValue => {
     try {
       setIsLoading(true);
-
       const QUERY_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}&q=${inputValue}`;
       const res = await fetch(QUERY_URL);
-
       if (!res.ok) {
         throw new Error("City not found!");
       }
       const data = await res.json();
       setCityData(data);
       setIsLoading(false);
+      setHasError(false);
     } catch (err) {
       setHasError(true);
       setIsLoading(false);
     }
   };
-
   function handleInputChange(e) {
     setInputValue(e.target.value);
   }
-
   function handleSubmit(e) {
     e.preventDefault();
     fetchData(inputValue);
