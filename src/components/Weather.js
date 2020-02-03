3777ba6168ed928a9ed9f82bb0cdffd53d5f0a8b
hikmeterdil <hikmetrdl@gmail.com>
2020-02-03 18:23:50 +0100

    fixed small mistakes

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -9,9 +9,9 @@ return(
 <h2 className="name" key={item.sys.id}>{item.name}, {item.sys.country}</h2>
 <h3 className="condition" key={item.sys.id}>{item.weather[0].main}</h3>
 <p className="condition" key={item.sys.id}>{item.weather[0].description}</p>
-<p className="para" key={item.sys.id}>{`min-temp: ${item.main.temp_min}`}</p>
-<p className="para" key={item.sys.id}>{`max-temp: ${item.main.temp_max}`}</p>
-<p className="para" key={item.sys.id}>{`location: ${item.coord.lon}, ${item.coord.lat}`}</p>
+<p className="para" key={item.sys.id}>min-temp: {item.main.temp_min}</p>
+<p className="para" key={item.sys.id}>max-temp: {item.main.temp_max}</p>
+<p className="para" key={item.sys.id}>location: {item.coord.lon}, {item.coord.lat}</p>
 </div>
 )}
 </div>
